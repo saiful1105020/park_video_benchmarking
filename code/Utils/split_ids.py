09b5e715c48646baf4cdfc625041d394a286420d
saiful1105020 <saiful.11722@gmail.com>
2026-02-26 19:10:15 -0500

    anonymity fix

--- code/Utils/split_ids.py
+++ code/Utils/split_ids.py
@@ -1,9 +1,21 @@
 import os
 import sys
 
+with open("../wandb_username.txt", "r") as f:
+    wandb_username = f.read().strip()
+
+with open("../project_name.txt", "r") as f:
+    project_name = f.read().strip()
+
+with open("../project_dir.txt", "r") as f:
+    project_dir = f.read().strip()
+
+with open("../protocol_name.txt", "r") as f:
+    protocol_name = f.read().strip()
+
 # copy dev and test IDs from the fusion project
-DEV_IDS_PATH = "/localdisk1/PARK/park_vlm/Dataset/dev_participant_ids.txt"
-TEST_IDS_PATH = "/localdisk1/PARK/park_vlm/Dataset/test_participant_ids.txt"
+DEV_IDS_PATH = f"/localdisk1/{project_dir}/{project_name}/code/Utils/dev_participant_ids.txt"
+TEST_IDS_PATH = f"/localdisk1/{project_dir}/{project_name}/code/Utils/test_participant_ids.txt"
 
 def get_dev_ids():
     with open(DEV_IDS_PATH, "r") as f:
